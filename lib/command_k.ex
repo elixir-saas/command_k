d7113f91811ea004816da7c2d5e85b8c4f461363
Justin Tormey <justin@upsidefinancing.com>
2023-02-22 08:40:04 -0600

    Implement live_view module

--- lib/command_k.ex
+++ lib/command_k.ex
@@ -3,16 +3,22 @@ defmodule CommandK do
   Documentation for `CommandK`.
   """
 
-  @doc """
-  Hello world.
+  defmacro __using__(opts) do
+    handler = Keyword.fetch!(opts, :handler)
 
-  ## Examples
-
-      iex> CommandK.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    quote do
+      on_mount({unquote(__MODULE__.LiveView), {:default, unquote(handler)}})
+    end
   end
+
+  ## Delegates
+
+  defdelegate open(), to: __MODULE__.LiveView
+  defdelegate close(), to: __MODULE__.LiveView
+  defdelegate toggle(), to: __MODULE__.LiveView
+
+  defdelegate send_open(), to: __MODULE__.LiveView
+  defdelegate send_close(), to: __MODULE__.LiveView
+  defdelegate send_toggle(), to: __MODULE__.LiveView
+  defdelegate send_exec(id, context), to: __MODULE__.LiveView
 end
---
+++ lib/command_k/live_view.ex
@@ -0,0 +1,78 @@
+defmodule CommandK.LiveView do
+  import Phoenix.Component
+  import Phoenix.LiveView
+
+  alias Phoenix.LiveView.JS
+
+  @prefix "@command_k:"
+  @show_key :__command_k_show
+  @handler_key :__command_k_handler
+
+  ## Mount
+
+  def on_mount({:default, handler}, _params, _session, socket) do
+    socket =
+      socket
+      |> assign(@show_key, false)
+      |> assign(@handler_key, handler)
+      |> attach_hook(:__command_k_event, :handle_event, &handle_event/3)
+      |> attach_hook(:__command_k_info, :handle_info, &handle_info/2)
+
+    {:cont, socket}
+  end
+
+  ## Events
+
+  def open(js \\ %JS{}), do: JS.push(js, @prefix <> "open")
+  def close(js \\ %JS{}), do: JS.push(js, @prefix <> "close")
+  def toggle(js \\ %JS{}), do: JS.push(js, @prefix <> "toggle")
+
+  defp handle_event(@prefix <> event, _params, socket) do
+    case event do
+      "open" ->
+        {:halt, do_open(socket)}
+
+      "close" ->
+        {:halt, do_close(socket)}
+
+      "toggle" ->
+        {:halt, do_toggle(socket)}
+    end
+  end
+
+  defp handle_event(_event, _params, socket), do: {:cont, socket}
+
+  ## Messsages
+
+  def send_open(), do: send(self(), {__MODULE__, :open})
+  def send_close(), do: send(self(), {__MODULE__, :close})
+  def send_toggle(), do: send(self(), {__MODULE__, :toggle})
+  def send_exec(id, context), do: send(self(), {__MODULE__, {:exec, id, context}})
+
+  defp handle_info({__MODULE__, message}, socket) do
+    case message do
+      :open ->
+        {:halt, do_open(socket)}
+
+      :close ->
+        {:halt, do_close(socket)}
+
+      :toggle ->
+        {:halt, do_toggle(socket)}
+
+      {:exec, id, context} ->
+        socket = get_handler(socket).handle_command(id, context, socket)
+        {:halt, do_close(socket)}
+    end
+  end
+
+  defp handle_info(_info, socket), do: {:cont, socket}
+
+  ## Helpers
+
+  defp do_open(socket), do: assign(socket, @show_key, true)
+  defp do_close(socket), do: assign(socket, @show_key, false)
+  defp do_toggle(socket), do: update(socket, @show_key, &(not &1))
+
+  defp get_handler(socket), do: socket.assigns[@handler_key]
+end
